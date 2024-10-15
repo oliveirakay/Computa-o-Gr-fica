44fc3f6e6d9484f7a363061eab8cfaa4d4a52517
Lucack <lucak260@gmail.com>
2024-10-15 09:49:30 -0300

    Outras configurações com input teclado

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ const float sizeStep = 0.01f;
 
 float red = 1.0f, green = 0.0f, blue = 0.0f;
 
-void desenhaQuadrado() {
+void desenhaQuadrado()
+{
     glColor3f(red, green, blue);
     glBegin(GL_QUADS);
     glVertex2f(-size + posX, -size + posY);
@@ -18,40 +19,93 @@ void desenhaQuadrado() {
     glEnd();
 }
 
-void teclado(unsigned char tecla, int x, int y) {
-    switch (tecla) {
-        case 'r':
-        case 'R':
-            red = 1.0f; green = 0.0f; blue = 0.0f;
-            break;
-        case 'g':
-        case 'G':
-            red = 0.0f; green = 1.0f; blue = 0.0f;
-            break;
-        case 'b':
-        case 'B':
-            red = 0.0f; green = 0.0f; blue = 1.0f;
-            break;   
+void tecladoEspecial(int tecla, int x, int y)
+{
 
-        case '+':
-            size += sizeStep;
-            break;
-        case '-':
-            size -= sizeStep;
-            size<0? size = 0: size = size;
+    switch (tecla)
+    {
+    case GLUT_KEY_UP:
+        posY += speed;
+        break;
+    case GLUT_KEY_DOWN:
+        posY -= speed;
+        break;
+    case GLUT_KEY_RIGHT:
+        posX += speed;
+        break;
+    case GLUT_KEY_LEFT:
+        posX -= speed;
+        break;
+    
+    case GLUT_KEY_HOME:
+        size = 0.1;
+        posX = -1+size;
+        posY =  1-size;
+        break;
+    case GLUT_KEY_END:
+        size = 0.1;
+        posX = 1-size;
+        posY =  -1+size;
+        break;
 
-            break; 
+    default:
+        break;
+    }
+
+    glutPostRedisplay();
+}
+
+void teclado(unsigned char tecla, int x, int y)
+{
+    switch (tecla)
+    {
+    case 'r':
+    case 'R':
+        red = 1.0f;
+        green = 0.0f;
+        blue = 0.0f;
+        break;
+    case 'g':
+    case 'G':
+        red = 0.0f;
+        green = 1.0f;
+        blue = 0.0f;
+        break;
+    case 'b':
+    case 'B':
+        red = 0.0f;
+        green = 0.0f;
+        blue = 1.0f;
+        break;
+
+    case '+':
+        size += sizeStep;
+        break;
+    case '-':
+        size -= sizeStep;
+        if (size < 0) size = 0;  // Garante que o size não fique negativo
+        break;
+
+    case 'c':
+    case 'C':
+        size = 0.1;
+        posX = 0;
+        posY = 0;
+
+        break;
     }
     glutPostRedisplay();
 }
 
-void renderiza() {
+void renderiza()
+{
     glClear(GL_COLOR_BUFFER_BIT);
     desenhaQuadrado();
     glFlush();
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(800, 600);
@@ -61,6 +115,7 @@ int main(int argc, char** argv) {
 
     glutDisplayFunc(renderiza);
     glutKeyboardFunc(teclado);
+    glutSpecialFunc(tecladoEspecial);
 
     glutMainLoop();
 
