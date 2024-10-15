c2dc52cd84a24668711f6af77036491081889edb
Lucack <lucak260@gmail.com>
2024-10-15 08:59:26 -0300

    Configuração FreeGlut

--- main.cpp
+++ main.cpp
@@ -1,37 +1,42 @@
-#include <GLFW/glfw3.h>
+#include <GL/glut.h>
 
-int main(void)
-{
-    GLFWwindow* window;
+float posX = 0.0f;
+float posY = 0.0f;
+float size = 0.1f;
+const float speed = 0.05f;
+const float sizeStep = 0.01f;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+float red = 1.0f, green = 0.0f, blue = 0.0f;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
+void desenhaQuadrado() {
+  glColor3f(red, green, blue);
+  glBegin(GL_QUADS);
+    glVertex2f(-size + posX, -size + posY);
+    glVertex2f(size + posX, -size + posY);
+    glVertex2f(size + posX, size + posY);
+    glVertex2f(-size + posX, size + posY);
+  glEnd();
 }
+
+void renderiza() {
+  glClear(GL_COLOR_BUFFER_BIT);
+  desenhaQuadrado();
+  glFlush();
+}
+
+int main(int argc, char** argv)
+ {
+  glutInit(&argc, argv);
+  glutInitWindowSize(800, 600);
+  glutCreateWindow("Quadrado e Teclado");
+
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+  glutDisplayFunc(renderiza);
+
+  glutMainLoop();
+
+  return 0;
+}
+//g++ -Wall -Iinclude -g main.cpp -o main.exe -Llib -lopengl32 -lglu32 -lfreeglut
+
